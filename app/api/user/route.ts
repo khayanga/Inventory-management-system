c8038f1a1fcf1ff50ae6dd4c7a6fabffbe222af6
khayanga <khayangavirgil@gmail.com>
2024-12-22 12:58:11 +0300

    Uer registration end point

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,74 @@
+import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+import * as z from "zod";
 
-export async function GET() {
 
-    return NextResponse.json({success:true})
-    
+// Define schema for form validation using zod
+const userSchema = z.object({
+    email: z.string().email(),
+    username: z.string().min(3),
+    password: z.string().min(6),
+});
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+        const { email, username, password } =userSchema.parse(body);
+
+        if (!email || !username || !password) {
+            return NextResponse.json(
+                { success: false, message: "All fields are required" },
+                { status: 400 }
+            );
+        }
+
+        // Check if the email exists
+        const existingUserByEmail = await db.user.findUnique({
+            where: { email },
+        });
+
+        if (existingUserByEmail) {
+            return NextResponse.json(
+                { user: null, message: "User with this email already exists" },
+                { status: 409 }
+            );
+        }
+
+        // Check if the username exists
+        const existingUserByName = await db.user.findUnique({
+            where: { username },
+        });
+
+        if (existingUserByName) {
+            return NextResponse.json(
+                { user: null, message: "User with this name already exists" },
+                { status: 409 }
+            );
+        }
+
+        const hashedPassword = await hash(password, 10);
+
+        // Insert new user into the database
+        const newUser = await db.user.create({
+            data: {
+                email,
+                username,
+                password : hashedPassword,
+                createdAt: new Date(),
+                updateAt: new Date(),
+            },
+        });
+
+        const{password:newUserPassword, ...rest} = newUser;
+
+        // Return success response with user details and timestamps
+        return NextResponse.json({user:rest , message: "User created successfully"}, {status: 201});
+    } catch (error) {
+        console.error("Error occurred in POST handler:", error);
+        return NextResponse.json(
+            { success: false || "Something went wrong" },
+            { status: 500 }
+        );
+    }
 }
---
+++ app/components/Signup.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <form>
+        {/* email input */}
+        <input type="email" placeholder="Email" />
+
+        {/* username input */}
+        <input type="text" placeholder="Username" />
+
+        {/* password input */}
+        <input type="password" placeholder="Password" />
+
+        {/* submit button */}
+    </form>
+  )
+};
+
+export default Signup
